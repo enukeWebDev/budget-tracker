51b8b959e5f1106f7c6324b230e719ed38f66220
Ren Guidolin <8.renji@gmail.com>
2022-02-15 17:28:45 -0500

    thinner navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,6 +3,8 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 import HomeIcon from '@mui/icons-material/Home';
+import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
+import HistoryIcon from '@mui/icons-material/History';
 
 
 function Navbar() {
@@ -10,10 +12,15 @@ function Navbar() {
     <main className="main-layout">
 
       <section className="navbar">
-        <h1> nav </h1>
-        <NavLink to="/home"> <HomeIcon fontSize="large" /> </NavLink> 
-        <NavLink to="/allowances"> <HomeIcon /> </NavLink>
-        <h2 to="/expenses"> Expense History </h2>
+          <div className='home'>
+            <NavLink to="/home"> <HomeIcon fontSize="large" /> </NavLink> 
+          </div>
+          <div className='allowances'>
+            <NavLink to="/allowances"> <PointOfSaleIcon fontSize='large' /> </NavLink>
+          </div>
+          <div className='expenses'>
+            <NavLink to="/expenses"> <HistoryIcon fontSize='large'/> </NavLink>
+          </div>
       </section>
 
   </main>
