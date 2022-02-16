71fe379423f970842715560d216096aa85ef4b0c
Roderick <enuke4@gmail.com>
2022-02-15 21:01:32 -0600

    update

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -24,17 +24,42 @@ export const AddTransaction = () => {
 
   return (
     <>
-      <h3 className="new-transaction">Add New Transaction</h3>
+      <h3>Add New Transaction</h3>
       <form onSubmit={onSubmit}>
 
+        {/* <div className="form-control">
+          <label htmlFor="text">Choose One...</label>
+          <input type="text" value={text} 
+          onChange={(e) => setText(e.target.value)} placeholder="Enter type of expenses..." /> */}
+
+
+        {/* This can be map instead later on to clean this up a bit */}
         <div className="form-control">
-          <label htmlFor="text">Text</label>
-          <input className="input-area-text-or-number" type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+          <select>
+            <option value="budget">Budget</option>
+            <option value="expense">Expense</option>
+          </select>
         </div>
+
         <div className="form-control">
-          <label className="amt" htmlFor="amount">Amount<br />
+          <select>
+            <option value="types">Please choose one...</option>
+            <option value="set-budget">Set you monthly budget</option>
+            <option value="bills">Bills</option>
+            <option value="food">Food</option>
+            <option value="gas">Gas</option>
+            <option value="entertainment">Entertainment</option>
+            <option value="fitness">Fitness</option>
+            <option value="others">Others</option>
+          </select>
+        </div>
+
+
+
+        <div className="form-controls">
+          <label htmlFor="amount">Please Enter Amount<br />
             (negative - expense, positive - budget)</label>
-          <input className="input-area-text-or-number" type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
 
         <button className="btn">Add Transaction</button>
