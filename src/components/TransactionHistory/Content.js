3aa954b7dd3e74f52840d735e0ee1f79bd0300c6
Ren Guidolin <8.renji@gmail.com>
2022-02-21 17:06:11 -0500

    transaction history shows price but now out of order

--- src/components/TransactionHistory/Content.js
+++ src/components/TransactionHistory/Content.js
@@ -1,15 +1,16 @@
 import Moment from 'react-moment';
 
 function Content(props) {
+  console.log('--------', props)
   return (
     <div className="transaction-content" onClick={props.form}>  
      <p>
        <span className="date">
-         <Moment format='dddd, MMMM DD yyyy'>{props.date}</Moment>
+         <Moment format='dddd, MMMM DD yyyy'>{props.transaction.date}</Moment>
        </span>
       </p>
-
-     <p className="cost">${props.amount} </p>
+      <p className="cost">{props.transaction.category} </p>
+     <p className="cost">${props.transaction.amount} </p>
 
     </div>
   );
