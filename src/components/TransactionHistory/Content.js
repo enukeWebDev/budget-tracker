5d47498448d0bf1ada7a353e74c87a0e5199ba84
Ren Guidolin <8.renji@gmail.com>
2022-02-22 01:38:29 -0500

    fixed header and working on cleaning up category display

--- src/components/TransactionHistory/Content.js
+++ src/components/TransactionHistory/Content.js
@@ -4,13 +4,15 @@ function Content(props) {
   console.log('--------', props)
   return (
     <div className="transaction-content" onClick={props.form}>  
-     <p>
-       <span className="date">
-         <Moment format='dddd, MMMM DD yyyy'>{props.transaction.date}</Moment>
-       </span>
-      </p>
-      <p className="cost">{props.transaction.category} </p>
-     <p className="cost">${props.transaction.amount} </p>
+
+     <div className='DOT'>
+         <Moment className="date" format='dddd, MMMM DD yyyy'>{props.transaction.date}</Moment>
+      </div>
+
+      <div className='cost-cat'>
+        <p className="category">{props.transaction.category} </p>
+        <p className="cost">${props.transaction.amount} </p>
+     </div>
 
     </div>
   );
