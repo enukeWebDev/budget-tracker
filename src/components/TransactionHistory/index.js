344b01dce15fd8d6f6836a5267fc3b012a9f3c41
Ren Guidolin <8.renji@gmail.com>
2022-02-21 23:05:42 -0500

    sort button added for display

--- src/components/TransactionHistory/index.js
+++ src/components/TransactionHistory/index.js
@@ -2,6 +2,9 @@ import './style.css';
 import Content from './Content';
 import { useContext } from 'react'
 import { GlobalContext } from '../../context/GlobalState';
+import DisplayOrder from './DisplayOrder';
+import ShowOnly from './ShowOnly';
+
 
 
 function Transaction() {
@@ -10,15 +13,21 @@ function Transaction() {
 
 
   return (
-    <div className='history'> 
-    <main className="transaction-layout" > 
-
-       <ul className="transaction-list">
-        { transactions.map(transaction => (<Content key={transaction.id} transaction={transaction} />)) }
-      </ul>
-      
-    </main>
-    </div>
+    <>
+      <h1>Transaction History</h1>
+      <div className='history'> 
+        <h1>Transaction History</h1>
+        <div className='sort-options'>
+        <DisplayOrder />
+        <ShowOnly />
+        </div>
+          <main className="transaction-layout" > 
+            <ul className="transaction-list">
+              { transactions.map(transaction => (<Content key={transaction.id} transaction={transaction} />)) }
+            </ul>
+          </main>
+      </div>
+    </>
   );
 }
 
